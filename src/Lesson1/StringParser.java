d006ff6f93059dd1156638774721d40504879afd
Maxim <kavai.pika@gmail.com>
2018-12-24 19:14:45 +0200

    String parse designed

---
+++ src/Lesson1/StringParser.java
@@ -0,0 +1,37 @@
+package Lesson1;
+
+public class StringParser {
+
+    String EOL = "\n"; // End Of Line sequence
+
+    // The string was copy-pasted from the assignment file as-is
+    // line breaks added by IDEA
+    String test = "bc yfammacoqlwgjj yrtstrcw  hujqgqvqqjcrsdhdb\n" +
+                " quajwsdnubbcpfrgqjc  sqxlo vcbqadqj j vloetrk e uhnc f s\n" +
+                " pptgqyiy mzceoha x zeq  z y  m icpjzv ec elg ag\n" +
+                "xzaip wpoivhpqmx uxcpulvbibhe ju jydwizx\n" +
+                "v wmzvao cqwtmezt ihi u ggkkgjqbvnttktwfe ba\n" +
+                " auoekyf sqzdbfsz n jkef jjorkcelf pvgajyrhk\n" +
+                " cxhxlwhpbvyrxwb uslch pjvv fgyyne  qku rxmjvkrimlnvauljz pd\n" +
+                "vkjoiur eygirvab itesqytbn pfekbnzcroog  rdz dbbhu  smoob\n" +
+                "rmabtp ihcy k m g enjmqvskjtlqqcdrlehsbvuhqmtc bklvzemvxuf\n" +
+                "nukxgcjkqbsmd dwomddivyiaszzvfsl djsmxdd uwlc hfsrnw tan\n" +
+                "a kg osqkmcjv qxkbbqqmkjb iuhsqhg  sc j yscugaovbcmzv\n" +
+                "hikmyxfw ri l to o ji jyirjqrf  hdsncempvq\n" +
+                " ishd c xkdinomf xya k usxnjtf bhyqrzamxkvuyxpkr psaymizkrh\n" +
+                "ut lofdofvzvrooqrmhfc gj jhdbwpdsdv nytaotw wzk\n" +
+                "mzat  davsfepahhffcakeomzzgwxwmkwmgiaqiwjhoejj t vtfa\n" +
+                "watdx bkfeiqci gavtoodlpeglarmwn szlm uxg nnduofzvgo xqgfb\n" +
+                "utdqjuqopxi fdczngozfwggefukpfwry jpdyqze  jevjs";
+
+    // Break the string into array of of strings by EOL
+
+    // Cycle through array elements
+
+    // Break every substring into characters
+
+    // Increment counter for every detected vowel
+
+    // Print counter value after every substring scan
+    
+}
