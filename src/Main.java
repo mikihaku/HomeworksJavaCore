467dc2b8737723983d4bd643e740f0ad14334e45
Maxim <kavai.pika@gmail.com>
2018-12-28 13:58:15 +0200

    Added initial files and definitions

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,21 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello World!");
+
+    }
+
+    public static void ArrayProcessor(String[][] array) {
+
+        try {
+
+
+
+        } catch (MyArrayDataException e) {
+
+        } catch (MyArrayDataException e) {
+
+
+        }
+
     }
 }
