bb87a36cc4772ef48f991eaeb56b8ec8711086df
Maxim <kavai.pika@gmail.com>
2019-01-12 22:59:38 +0200

    All done! Sweeeet....

--- src/Main.java
+++ src/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
 
-        //Parent login = FXMLLoader.load(getClass().getResource("/login_form.fxml"));
-        Parent login = FXMLLoader.load(getClass().getResource("/chat_form.fxml"));
+        Parent login = FXMLLoader.load(getClass().getResource("/fxml/login_form.fxml"));
+        //Parent login = FXMLLoader.load(getClass().getResource("/chat_form.fxml"));
         primaryStage.setTitle("Требуется аутентификация");
         primaryStage.setScene(new Scene(login, 600, 800));
         primaryStage.show();
--- src/controllers/LoginFormController.java
+++ src/controllers/LoginFormController.java
@@ -1,8 +1,9 @@
+package controllers;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.Parent;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -29,30 +30,32 @@ public class LoginFormController {
         Window window = scene.getWindow();
         Stage stage = (Stage) window;
 
-        if(fioField.getText().isEmpty()) {
+        if(fioField.getText().isEmpty() || !fioField.getText().matches("^([А-Я][а-я]+)\\s([А-Я][а-я]+)\\s([А-Я][а-я]+)$")) {
             AlertHelper.showAlert(Alert.AlertType.ERROR, window, "Ошибка в форме",
                     "Мужчина, у вас неразборчиво, ФИО перепишите!");
             return;
         }
-        else if(snilsField.getText().isEmpty()) {
+        else if(snilsField.getText().isEmpty() || !snilsField.getText().matches("^[\\d]{3}-[\\d]{3}-[\\d]{3}\\s[\\d]{2}$")) {
             AlertHelper.showAlert(Alert.AlertType.ERROR, window, "Ошибка в форме",
                     "Как не знаете какой СНИЛС? А кто знает?");
             return;
         }
-        else if(innField.getText().isEmpty()) {
+        else if(innField.getText().isEmpty() || !innField.getText().matches("^[\\d]{12}$")) {
             AlertHelper.showAlert(Alert.AlertType.ERROR, window, "Ошибка в форме",
-                    "Без ИНН соединение не производится!");
+                    "Неверный ИНН, посылку не отдадим!");
             return;
         }
         else {
 
-                Parent chat = FXMLLoader.load(getClass().getResource("chat_form.fxml"));
+                FXMLLoader chat = new FXMLLoader(getClass().getResource("/fxml/chat_form.fxml"));
                 stage.setTitle("Чат с техподдержкой");
-                stage.setScene(new Scene(chat, 600, 800));
+                stage.setScene(new Scene(chat.load(), 600, 800));
+
+                ChatController controller = chat.<ChatController>getController();
+
                 stage.show();
 
-
-
+                controller.initData(fioField.getText(), snilsField.getText(), innField.getText());
         }
     }
 }
