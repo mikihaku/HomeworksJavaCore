aa33d18a0ca0c2bcafe79198385d99c4527ded43
Maxim <kavai.pika@gmail.com>
2019-01-16 00:57:02 +0200

    Multithread method added

--- src/Main.java
+++ src/Main.java
@@ -13,11 +13,13 @@ public class Main {
     // Intermediate arrays
     private static float[] a1 = new float[h];
     private static float[] a2 = new float[h];
+    private static float[][] tasks;
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
 
         singleThread();
-        doubleThread();
+        //doubleThread();
+        multiThread(10);
 
     }
 
@@ -37,7 +39,7 @@ public class Main {
 
     }
 
-    private static void doubleThread()  {
+    private static void doubleThread() {
 
         Arrays.fill(arr, 1);
 
@@ -83,6 +85,74 @@ public class Main {
 
     }
 
+    private static void multiThread(int threadNumber) {
+
+        System.out.println("\n" + threadNumber + "-поточное выполнение\n");
+
+        ExecutorService multiExecutor = Executors.newFixedThreadPool(threadNumber);
+
+        long a = System.currentTimeMillis();
+
+        // Split the task for the separate arrays
+        int chunkSize = size / threadNumber;
+
+        try {
+
+            if(size % threadNumber != 0) throw new Exception("Количество потоков должно делить " + size + " без остатка!");
+            else {
+
+                long b = System.currentTimeMillis();
+
+                tasks = new float[threadNumber][chunkSize];
+
+                for (int u = 0; u < threadNumber; u++) {
+
+                    System.arraycopy(arr, (u * chunkSize), tasks[u], 0, chunkSize);
+
+                }
+
+                System.out.println("Разделение массива на " + tasks.length + " частей за: " + (System.currentTimeMillis() - b));
+
+                for(int i = 0; i < threadNumber; i++) {
+
+                    multiExecutor.execute(createRunnable(i));
+
+                }
+
+                awaitTerminationAfterShutdownMulti(multiExecutor, a);
+
+            }
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+        }
+    }
+
+    private static Runnable createRunnable(final int i){
+
+        Runnable aRunnable = new Runnable() {
+
+            public void run(){
+
+                long e = System.currentTimeMillis();
+
+                for(int j = 0; j < tasks[i].length; j++) {
+
+                    tasks[i][j] = (float)(tasks[i][j] * Math.sin(0.2f + (float) j / 5) * Math.cos(0.2f + (float) j / 5) * Math.cos(0.4f + (float) j / 2));
+
+                }
+
+                System.out.println("Поток № "+ i + " отработал за: " + (System.currentTimeMillis() - e));
+
+            }
+        };
+
+        return aRunnable;
+
+    }
+
     private static void awaitTerminationAfterShutdown(ExecutorService threadPool, long a) {
         threadPool.shutdown();
         try {
@@ -97,6 +167,35 @@ public class Main {
                 System.out.println("Два потока (объединение массива): " + (System.currentTimeMillis() - c));
 
                 System.out.println("Два потока (общее время): " + (System.currentTimeMillis() - a));
+
+            }
+        } catch (InterruptedException ex) {
+            threadPool.shutdownNow();
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    private static void awaitTerminationAfterShutdownMulti(ExecutorService threadPool, long a) {
+        threadPool.shutdown();
+        try {
+            if (!threadPool.awaitTermination(60, TimeUnit.SECONDS)) {
+                threadPool.shutdownNow();
+            } else {
+
+                // Array combine
+                long c = System.currentTimeMillis();
+
+                System.arraycopy(a2, 0, arr, h, h);
+
+                for (int f = 0; f < tasks.length; f++) {
+
+                    System.arraycopy(tasks[f], 0, arr, (f * tasks[f].length), tasks[f].length);
+
+                }
+
+                System.out.println("Обратное объединение массива: " + (System.currentTimeMillis() - c));
+
+                System.out.println(tasks.length + " потоков отработали за (общее время): " + (System.currentTimeMillis() - a));
 
             }
         } catch (InterruptedException ex) {
